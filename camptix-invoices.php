d1d0de7b697b8b7d81dbd66b6e3a80f638a291b4
Bahuaud Willy <>
2018-01-08 22:35:11 +0100

    supprimé le fichier `options-page` (tout dans class plugin)

--- camptix-invoices.php
+++ camptix-invoices.php
@@ -13,21 +13,72 @@ if ( ! defined( 'ABSPATH' ) ) {
 define( 'CTX_INV_VER', time() );
 
 /**
- * All settings functions are in this file
- */
-require 'options-page.php';
-
-/**
  * Load invoice addon
  */
 add_action( 'camptix_load_addons', __NAMESPACE__ . '\load' );
 function load() {
 	class CampTix_Addon_Invoices extends \CampTix_Addon {
+		/**
+		 * Init invoice addon
+		 */
 		function camptix_init() {
 			global $camptix;
-			add_filter( 'camptix_setup_sections', __NAMESPACE__ . '\invoice_settings_tab' );
-			add_action( 'camptix_menu_setup_controls', __NAMESPACE__ . '\invoice_settings' );
-			add_filter( 'camptix_validate_options', __NAMESPACE__ . '\validate_options', 10, 2 );
+			add_filter( 'camptix_setup_sections', array( __CLASS__, 'invoice_settings_tab' ) );
+			add_action( 'camptix_menu_setup_controls', array( __CLASS__, 'invoice_settings' ) );
+			add_filter( 'camptix_validate_options', array( __CLASS__, 'validate_options' ), 10, 2 );
+		}
+
+		/**
+		 * Add a new tab in camptix settings
+		 */
+		static function invoice_settings_tab( $sections ) {
+			$sections['invoice'] = __( 'Facturation' );
+			return $sections;
+		}
+
+		/**
+		 * Tab content
+		 */
+		static function invoice_settings( $section ) {
+			if ( 'invoice' !== $section ) {
+				return false;
+			}
+			$opt = get_option( 'camptix_options' );
+			add_settings_section( 'invoice', __( 'Réglages des factures' ), '__return_false', 'camptix_options' );
+			global $camptix;
+			$camptix->add_settings_field_helper( 'invoice-new-year-reset', 'Réinitialisation annuelle', 'field_yesno' ,'invoice', 
+				sprintf( __( 'Les numéros de facture sont préfixés par l’année, et seront réinitialisés le premier janvier. (ex: %1$s-125)' ), date( 'Y' ) )
+			);
+			add_settings_field( 'invoice-current-number', 'Prochaine facture', array( __CLASS__, 'current_number' ), 'camptix_options', 'invoice', array(
+				'id'    => 'invoice-current-number',
+				'value' => isset( $opt['invoice-current-number'] ) ? $opt['invoice-current-number'] : 1,
+				'yearly' => isset( $opt['invoice-new-year-reset'] ) ? $opt['invoice-new-year-reset'] : false
+			) );
+		}
+
+		/**
+		 * Next invoice number setting
+		 */
+		static function current_number_callback( $args ) {
+			vprintf( '<p>' . __( 'La prochaine facture portera le numéro' ) . ' %3$s<input type="number" min="1" value="%2$d" name="camptix_options[%1$s]" class="small-text">%4$s</p>', array(
+				esc_attr( $args['id'] ),
+				esc_attr( $args['value'] ),
+				$args['yearly'] ? '<code>' . date( 'Y-' ) : '',
+				$args['yearly'] ? '</code>' : '',
+			) );
+		}
+
+		/**
+		 * Validate our custom options
+		 */
+		static function validate_options( $output, $input ) {
+			if ( isset( $input['invoice-new-year-reset'] ) ) {
+				$output['invoice-new-year-reset'] = intval( $input['invoice-new-year-reset'] );
+			}
+			if ( ! empty( $input['invoice-current-number'] ) ) {
+				$output['invoice-current-number'] = (int) $input['invoice-current-number'];
+			}
+			return $output;
 		}
 	}
 	camptix_register_addon( __NAMESPACE__ . '\CampTix_Addon_Invoices' );
